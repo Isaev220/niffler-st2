b42d26fd16b9c7e0638adc61f20a801eca85b411
isaev <isaevantony220@yandex.ru>
2023-04-26 17:57:30 +0300

    HomeWork1

--- niffler-e-2-e-tests/src/test/java/niffler/test/BaseWebTest.java
+++ niffler-e-2-e-tests/src/test/java/niffler/test/BaseWebTest.java
@@ -6,7 +6,9 @@ import niffler.jupiter.annotation.WebTest;
 
 @WebTest
 public abstract class BaseWebTest {
+  static final String USER = "anton";
 
+  static final String PASSWORD = "1234";
   static {
     Configuration.browserSize = "1920x1080";
   }
--- niffler-e-2-e-tests/src/test/java/niffler/test/SpendsWebTest.java
+++ niffler-e-2-e-tests/src/test/java/niffler/test/SpendsWebTest.java
@@ -7,6 +7,7 @@ import static com.codeborne.selenide.Selenide.$$;
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Selenide;
 import io.qameta.allure.AllureId;
+import niffler.jupiter.annotation.GenerateCategory;
 import niffler.jupiter.annotation.GenerateSpend;
 import niffler.model.CurrencyValues;
 import niffler.model.SpendJson;
@@ -14,28 +15,31 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled
 public class SpendsWebTest extends BaseWebTest {
 
     @BeforeEach
     void doLogin() {
         Selenide.open("http://127.0.0.1:3000/main");
         $("a[href*='redirect']").click();
-        $("input[name='username']").setValue("dima");
-        $("input[name='password']").setValue("12345");
+        $("input[name='username']").setValue(USER);
+        $("input[name='password']").setValue(PASSWORD);
         $("button[type='submit']").click();
     }
 
+    @GenerateCategory(
+            username = USER,
+            category = "fitness"
+    )
     @GenerateSpend(
-        username = "dima",
-        description = "QA GURU ADVANCED VOL 2",
-        currency = CurrencyValues.RUB,
-        amount = 52000.00,
-        category = "Обучение"
+            username = USER,
+            description = "QA GURU ADVANCED VOL 2",
+            currency = CurrencyValues.RUB,
+            amount = 50000.00,
+            category = "fitness"
     )
     @AllureId("101")
     @Test
-    void spendShouldBeDeletedByActionInTable(SpendJson spend) {
+    public void spendShouldBeDeletedByActionInTable(SpendJson spend) {
         $(".spendings-table tbody").$$("tr")
             .find(text(spend.getDescription()))
             .$$("td").first()
@@ -48,6 +52,5 @@ public class SpendsWebTest extends BaseWebTest {
         $(".spendings-table tbody")
             .$$("tr")
             .shouldHave(CollectionCondition.size(0));
-        throw new IllegalStateException();
     }
 }
